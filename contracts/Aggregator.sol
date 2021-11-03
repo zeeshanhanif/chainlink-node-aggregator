31d167836005c59a694255569d6714b59f9d7853
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-03 14:36:02 +0500

    Added Aggregator implementation

--- contracts/Aggregator.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV2V3Interface.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-abstract contract Aggregator is AggregatorV2V3Interface, Ownable {
-}
