50ebe6f4254116ae10789a3ea36c3834fb4ac50b
zeeshanhanif <zeeshanhanif@gmail.com>
2021-11-09 11:21:52 +0500

    Added scripts for aggregator funding and adding oracle

---
+++ scripts/03_update-internal-funding.ts
@@ -0,0 +1,34 @@
+import { BigNumber } from '@ethersproject/bignumber';
+import { ethers, run } from 'hardhat';
+import { Median, Median__factory, MyAggregator, MyAggregator__factory, Oracle, Oracle__factory } from '../typechain';
+
+async function main() {
+  const accounts = await ethers.getSigners();
+
+  const Median:Median__factory = await ethers.getContractFactory("Median");
+  const median:Median = await Median.attach("0x831884a02fac4F0d6dBFA4C14a7D5834211b7762");
+
+  const Aggregator:MyAggregator__factory = await ethers.getContractFactory("MyAggregator",{
+    libraries: {
+      Median:median.address
+    }
+  });
+  
+  const aggregator:MyAggregator = await Aggregator.attach("0xAFE14D93e156CcCBca754d205A9ea925510ff127");
+  console.log("Updated Avaiable funds ",(await aggregator.availableFunds()).toString());
+  console.log("Allocated funds ",(await aggregator.allocatedFunds()).toString());
+
+  await aggregator.updateAvailableFunds();
+
+  console.log("Updated Avaiable funds ",(await aggregator.availableFunds()).toString());
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
